7c82027d9197c91439864b66a830137d715ddc50
student <kalluru.sushma@hcl.com>
2021-04-21 12:23:31 +0000

    first commit

---
+++ FirstProject/src/test/java/com/hcl/sushma/springboot/FirstProject/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.sushma.springboot.FirstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
